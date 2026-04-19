bb20f16ceb472ebb77b2468ad048e1f304b856f3
SrCatFr2 <sr.cat.accs@gmail.com>
2026-04-19 10:28:44 -0700

    Update Message.js

--- src/models/Message.js
+++ src/models/Message.js
@@ -1,74 +1,68 @@
 import mongoose from 'mongoose';
 
-const messageSchema = new mongoose.Schema({
-  author: {
-    name: {
+const messageSchema = new mongoose.Schema(
+  {
+    author: {
+      name: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 50
+      },
+      userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        default: null
+      },
+      avatar: {
+        type: String,
+        default: null
+      }
+    },
+    content: {
       type: String,
       required: true,
       trim: true,
-      maxlength: 50
+      maxlength: 5000
     },
-    userId: {
+    images: [
+      {
+        type: String
+      }
+    ],
+    videos: [
+      {
+        type: String
+      }
+    ],
+    parentId: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
+      ref: 'Message',
       default: null
+    },
+    replies: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Message'
+      }
+    ],
+    likes: {
+      type: Number,
+      default: 0,
+      min: 0
+    },
+    edited: {
+      type: Boolean,
+      default: false
     }
   },
-  content: {
-    type: String,
-    required: true,
-    trim: true,
-    maxlength: 5000
-  },
-  images: [{
-    type: String,
-    validate: {
-      validator: function(url) {
-        return /^https?:\/\/.+/.test(url);
-      },
-      message: 'URL de imagen inválida'
-    }
-  }],
-  videos: [{
-    type: String,
-    validate: {
-      validator: function(url) {
-        return /^https?:\/\/.+/.test(url);
-      },
-      message: 'URL de video inválida'
-    }
-  }],
-  parentId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Message',
-    default: null
-  },
-  replies: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Message'
-  }],
-  likes: {
-    type: Number,
-    default: 0,
-    min: 0
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now
-  },
-  edited: {
-    type: Boolean,
-    default: false
+  {
+    timestamps: true
   }
-}, { timestamps: true });
+);
 
-// Indexar para mejor rendimiento
 messageSchema.index({ createdAt: -1 });
 messageSchema.index({ parentId: 1 });
-messageSchema.index({ 'author.userId': 1 });
+messageSchema.index({ "author.userId": 1 });
 
-export default mongoose.model('Message', messageSchema);
+export default mongoose.models.Message || mongoose.model("Message", messageSchema);
