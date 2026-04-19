d3cecda9fe454a4f7e05ba6e17ca473b88ee6467
SrCatFr2 <sr.cat.accs@gmail.com>
2026-04-18 23:27:25 -0700

    Update Message.js

--- src/models/Message.js
+++ src/models/Message.js
@@ -1,19 +1,74 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const messageSchema = new mongoose.Schema({
   author: {
-    name: { type: String, required: true },
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 50
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      default: null
+    }
   },
-  content: { type: String, required: true },
-  images: [{ type: String }], // URLs de Cloudinary
-  videos: [{ type: String }], // URLs de Cloudinary
-  parentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Message' }, // Para respuestas
-  replies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Message' }],
-  likes: { type: Number, default: 0 },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
-  edited: { type: Boolean, default: false }
-});
+  content: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 5000
+  },
+  images: [{
+    type: String,
+    validate: {
+      validator: function(url) {
+        return /^https?:\/\/.+/.test(url);
+      },
+      message: 'URL de imagen inválida'
+    }
+  }],
+  videos: [{
+    type: String,
+    validate: {
+      validator: function(url) {
+        return /^https?:\/\/.+/.test(url);
+      },
+      message: 'URL de video inválida'
+    }
+  }],
+  parentId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Message',
+    default: null
+  },
+  replies: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Message'
+  }],
+  likes: {
+    type: Number,
+    default: 0,
+    min: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  },
+  edited: {
+    type: Boolean,
+    default: false
+  }
+}, { timestamps: true });
 
-module.exports = mongoose.model('Message', messageSchema);
+// Indexar para mejor rendimiento
+messageSchema.index({ createdAt: -1 });
+messageSchema.index({ parentId: 1 });
+messageSchema.index({ 'author.userId': 1 });
+
+export default mongoose.model('Message', messageSchema);
